a039e6cb56cc29c50d34110b0bb5e3f7bdcd1e7f
johnfitzpatrick <john.fitzpatrick@rightscale.com>
2012-09-24 11:24:36 +0000

    added to github

---
+++ cookbooks/hello_world123/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: hello_world123
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
---
+++ cookbooks/jaf_chef/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: jaf_chef
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+logs "johns log"
